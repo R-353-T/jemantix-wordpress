8b2d498a49a49c680c7294450f936b3937b70454
Adrien Villalonga <nd4yza9cgmdx4cxpq4box6tv7nh80zgc@proton.me>
2024-09-02 20:54:15 +0200

    1.0.3

--- source/php/component/download.php
+++ source/php/component/download.php
@@ -8,22 +8,36 @@
 <div class="download">
     <h3>Description</h3>
     <p>
-        Dark theme et leaderboard pour le jeu du Cémantix & Pédantix.<br>
-        Vous ne pourriez pas me faire un plus beau cadeau en défiant vos amis avec.
+    Jemantix est une extension qui à été conçue pour optimiser votre expérience des jeux Cémantix et Pédantix 
+    (<a href="https://cemantix.certitudes.org/">https://cemantix.certitudes.org/</a>),
+    qui défient votre intelligence et votre maîtrise du langage.
+    <br>
+    <br>
+    Grâce à cette extension, l'ergonomie de ces jeux est grandement améliorée,
+    vous permettant de vous concentrer pleinement sur votre partie
+    et partager votre score avec la communauté.
+    </p>
+
+    <p>
+        Merci d'avance pour vos retours.
         <br>
-        Merci d'avance.
         <br>
-        <br>
-        R353T_.
+        R353T.
     </p>
 
     <h3>Téléchargement</h3>
+
+    <h4>Version actuelle: <u>4.2.2</u> (02/09/2024 - 20h44)</h4>
+
     <div>
         <a href="https://addons.mozilla.org/fr/firefox/addon/jemantix"
             target="_blank" class="firefox">Firefox (Bureau & Mobile Android)</a>
         <a href="https://chromewebstore.google.com/detail/jemantix/klmgknmplonlpacgdaafnjpekdfcchjd"
             target="_blank" class="chrome">Chrome et Opera (Bureau)</a>
     </div>
+
+    <p class="warning">Les mises à jours ne sont pas automatiques.</p>
+    <p class="info">Des bugs ou une idée ? Merci de m'en informer par email ou via les 'issues' github Merci.</p>
 
     <h3>Code source</h3>
     <div>
